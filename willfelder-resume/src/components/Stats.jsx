89c589ea41732918422bb3ffc63c8b5f682f53ab
William Lengenfelder <william.lengenfelder@gmail.com>
2022-10-04 09:20:47 -0300

    added Stats component

--- willfelder-resume/src/components/Stats.jsx
+++ willfelder-resume/src/components/Stats.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import styles from "../style";
+import { stats } from "../constants";
 
 const Stats = () => {
+
     return(
-        <div>Stats</div>
+        <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+            {stats.map((stat) => (
+                <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+                    <h4 className="text-white font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]">
+                        {stat.value}
+                    </h4>
+                    <p className="text-gradient font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] uppercase ml-3">
+                        {stat.title}
+                    </p>
+                </div>
+            ))}
+        </section>
     )
 };
 
