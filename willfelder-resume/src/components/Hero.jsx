9dd7b123b7d2e036e38b5041404670cec7f659ef
William Lengenfelder <william.lengenfelder@gmail.com>
2022-10-10 04:55:57 -0300

    update project

--- willfelder-resume/src/components/Hero.jsx
+++ willfelder-resume/src/components/Hero.jsx
@@ -10,29 +10,13 @@ const Hero = () => {
 
             <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
 
-                <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-
-                    <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-                    <p className={`${styles.paragraph} ml-2`}>
-                        <span className="text-white">20%</span>
-                        Discount for {" "}
-                        <span className="text-white">1 Month</span>
-                        Account
-                    </p>
-                    
-                </div>
-
                 <div className="flex flex-row justify-between items-center w-full">
-                    <h1 className="text-white flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px]">
-                        The Next <br className="sm:block hidden" />{" "}
-                        <span className="text-gradient">Generation</span>{" "}
+                    <h1 className="text-white flex-1 font-poppins font-semibold ss:text-[72px] text-[22px] ss:leading-[80px] leading-[75px]">
+                        Hi, I am <br className="sm:block hidden" />{" "}
+                        <span className="text-gradient">William Lengenfelder</span>{" "}
                     </h1>
-                    <div className="ss:flex hidden md:mr-4 mr-0">
-                        <GetStarted />
-                    </div>
                 </div>
 
-                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] w-full">Payment Method.</h1>
                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
             </div>
 
@@ -41,10 +25,6 @@ const Hero = () => {
                 <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
                 <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
                 <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-            </div>
-
-            <div className={`ss:hidden ${styles.flexCenter}`}>
-                <GetStarted />
             </div>
         </section>
     )
--- willfelder-resume/src/components/Navbar.jsx
+++ willfelder-resume/src/components/Navbar.jsx
@@ -8,8 +8,9 @@ const Navbar = () => {
 
     return(
         <nav className="w-full flex py-6 justify-between items-center navbar">
-
-            <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+            <a href={"#home"}>
+                <img src={logo} alt="hoobank" className="w-[32px] h-[32px]" />
+            </a>
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
                 {navLinks.map((nav, index) => (
                     <li 
